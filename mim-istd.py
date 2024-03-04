da105f88c5745ca50bcd60147c176f495a2714a5
freq <55972768+txchen-USTC@users.noreply.github.com>
2024-03-04 11:15:16 +0800

    Add files via upload

--- mim-istd.py
+++ mim-istd.py
@@ -656,64 +656,7 @@ class Stage(nn.Module):
             inner_tokens, outer_tokens = blk(inner_tokens, outer_tokens, H_out, W_out, H_in, W_in, self.relative_pos)
         return inner_tokens, outer_tokens
 
-#######################################################################
-up_kwargs = {'mode': 'bilinear', 'align_corners': False}
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    conv3x3 = nn.Sequential(
-        nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                  padding=1, bias=False),
-        nn.BatchNorm2d(out_planes),
-        nn.ReLU(inplace=True),
-    )
-    return conv3x3
-class SegHead(nn.Module):
-    def __init__(self, in_channels=[96, 192, 384, 768], num_classes=1, in_index=[0, 1, 2, 3]):
-        super(SegHead, self).__init__()
-        self.in_index = in_index
 
-        self.conv1 = conv3x3(in_channels[0], in_channels[0])
-        self.conv2 = conv3x3(in_channels[1], in_channels[0])
-        self.conv3 = conv3x3(in_channels[2], in_channels[0])
-        self.conv4 = conv3x3(in_channels[3], in_channels[0])
-        self.final_layer = nn.Sequential(
-            nn.Conv2d(
-                in_channels=in_channels[0] * 4,
-                out_channels=in_channels[0] * 4,
-                kernel_size=1,
-                stride=1,
-                padding=0),
-            nn.BatchNorm2d(in_channels[0] * 4),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(
-                in_channels=in_channels[0] * 4,
-                out_channels=num_classes,
-                kernel_size=1,
-                stride=1,
-                padding=0)
-        )
-
-    def _transform_inputs(self, inputs):
-        if isinstance(self.in_index, (list, tuple)):
-            inputs = [inputs[i] for i in self.in_index]
-        elif isinstance(self.in_index, int):
-            inputs = inputs[self.in_index]
-        return inputs
-
-    def forward(self, inputs):
-        inputs = self._transform_inputs(inputs)
-        p2, p3, p4, p5 = inputs
-        h, w = p2.shape[-2:]
-        x2 = self.conv1(p2)
-        x3 = F.interpolate(self.conv2(p3), size=(h, w), **up_kwargs)
-        x4 = F.interpolate(self.conv3(p4), size=(h, w), **up_kwargs)
-        x5 = F.interpolate(self.conv4(p5), size=(h, w), **up_kwargs)
-
-        x = torch.cat((x2, x3, x4, x5), dim=1)
-        x = self.final_layer(x)
-
-        return x
-##################################################3
 class UpsampleBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UpsampleBlock, self).__init__()
@@ -817,11 +760,6 @@ class PyramidMiM(nn.Module):
         for i in range(4):
             self.up_blocks.append(UpsampleBlock(outer_dims[i],outer_dims[i]))
            
-
-
-
-
-        #self.decode_head = SegHead(in_channels=[128,256,512,1024], num_classes=1, in_index=[0, 1, 2, 3])
 
         trunc_normal_(self.outer_pos, std=.02)
         trunc_normal_(self.inner_pos, std=.02)
